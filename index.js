15107d7812571f38e6c2ee920d9dd822a7b23e9e
ereti <43418802+ereti@users.noreply.github.com>
2020-12-08 05:48:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+process.env.NODE_ENV = "production";
+
+//globals
+global.CONFIG = require("config.json");
+global.DB     = require("./lib/db.js");
+
+//packages
+const express = require("express");
+
+//express config
+const app     = express();
+app.set("json spaces", 2);
+
+//routing
+app.use("/ir", require("./ir/"));
+
+
+app.listen(global.CONFIG.serverPort, _ => console.log(`Server listening on port ${global.CONFIG.serverPort}`));
---
+++ ir/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const IRService = require("../services/IRService.js");
+
+const router = express.Router();
+
+router.use(IRService.Authorize);
+
+//Heartbeat
+router.get("/",                         IRService.Heartbeat);
+router.get("/charts/:chartHash",        IRService.ChartTracked);
+router.get("/charts/:chartHash/record", IRService.Record);
+router.get("/score/submit",             IRService.SubmitScore);
+
+module.exports = router;
