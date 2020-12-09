98d1a9d3dead42ff9e60489c2aba9c92b924fb1c
ereti <43418802+ereti@users.noreply.github.com>
2020-12-09 23:52:06 +0000

    Move express-session init + use into routes so it can come after IR
    No need for IR requests to be slowed down by WebUI session checking

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ global.DB     = require("./lib/db.js");
 
 //packages
 const express = require("express");
-const express_session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(express_session);
 
 //express config
 const app     = express();
@@ -19,24 +17,7 @@ if(global.CONFIG.https)
                                         //if you are having problems with cookies not being set, you're probably doing something more complicated
                                         //and need to adjust this: e.g. the reverse proxy being on a non-local IP
 
-//session config
-const session = express_session({
-    name: "webui_session",
-    secret: global.CONFIG.webUICookieSecret,
-    store: new MongoDBStore({
-        uri: `mongodb://${global.CONFIG.database}/uscir`,
-        collection: "sessions"
-    }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: global.CONFIG.https, //can't use secure if not using https
-        maxAge: 1000 * 60 * 60 * 24 * 7 * 4 //1 month
-    }
-})
-
 //app setup
-app.use(session);
 app.use(require("./routes/"));
 
 //start listening
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const express_session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(express_session);
+
+//session config
+const session = express_session({
+    name: "webui_session",
+    secret: global.CONFIG.webUICookieSecret,
+    store: new MongoDBStore({
+        uri: `mongodb://${global.CONFIG.database}/uscir`,
+        collection: "sessions"
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: global.CONFIG.https, //can't use secure if not using https
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 4 //1 month
+    }
+})
 
 const router = express.Router();
 
 router.use(bodyparser.json());
 
+//ir (i.e. usc) endpoints
 router.use("/ir", require("./ir/"));
-router.use("/auth", require("./auth/"));
 
+//use session after ir because ir doesn't need our webui sessions
+router.use(session);
+
+//everything below = webui endpoints
+
+router.use("/auth", require("./auth/"));
 router.get("/", (req, res) => res.render("index", {username: req.session.user.username}));
 
 module.exports = router;
