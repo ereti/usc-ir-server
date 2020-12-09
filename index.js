67802e66f94956eb8370310992bbd7e25b62c304
ereti <43418802+ereti@users.noreply.github.com>
2020-12-09 22:12:09 +0000

    Add config options relevant to authentication - set trust proxy if https

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const express = require("express");
 //express config
 const app     = express();
 app.set("json spaces", 2);
+if(global.CONFIG.https)
+    app.set("trust proxy", "loopback"); //trust proxy for https.
+                                        //if you are having problems with cookies not being set, you're probably doing something more complicated
+                                        //and need to adjust this: e.g. the reverse proxy being on a non-local IP
 
 //routing
 app.use("/ir", require("./ir/"));
