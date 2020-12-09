8cdca65509ca16d2c96d7371b0603936def21ac1
ereti <43418802+ereti@users.noreply.github.com>
2020-12-09 23:29:24 +0000

    Render index page

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,6 @@ router.use(bodyparser.json());
 router.use("/ir", require("./ir/"));
 router.use("/auth", require("./auth/"));
 
+router.get("/", (req, res) => res.render("index", {username: req.session.user.username}));
+
 module.exports = router;
