be9382288d4abc3f7865231b4320626f6e37cc89
ereti <43418802+ereti@users.noreply.github.com>
2020-12-09 23:24:05 +0000

    Set up sessions in index.js

--- index.js
+++ index.js
@@ -6,17 +6,37 @@ global.DB     = require("./lib/db.js");
 
 //packages
 const express = require("express");
+const express_session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(express_session);
 
 //express config
 const app     = express();
 app.set("json spaces", 2);
+app.set("view engine", "ejs")
 if(global.CONFIG.https)
     app.set("trust proxy", "loopback"); //trust proxy for https.
                                         //if you are having problems with cookies not being set, you're probably doing something more complicated
                                         //and need to adjust this: e.g. the reverse proxy being on a non-local IP
 
-//routing
-app.use("/ir", require("./ir/"));
+//session config
+const session = express_session({
+    name: "webui_session",
+    secret: global.CONFIG.webUICookieSecret,
+    store: new MongoDBStore({
+        uri: `mongodb://${global.CONFIG.database}/uscir`,
+        collection: "sessions"
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: global.CONFIG.https, //can't use secure if not using https
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 4 //1 month
+    }
+})
 
+//app setup
+app.use(session);
+app.use(require("./routes/"));
 
+//start listening
 app.listen(global.CONFIG.serverPort, _ => console.log(`Server listening on port ${global.CONFIG.serverPort}`));
