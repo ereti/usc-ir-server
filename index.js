e2a2af458ebc993c0d479ce77cb24df3ddfeb541
ereti <43418802+ereti@users.noreply.github.com>
2020-12-11 01:56:52 +0000

    Set config in app.locals for ejs usage

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ const express = require("express");
 
 //express config
 const app     = express();
+
 app.set("json spaces", 2);
 app.set("view engine", "ejs")
 app.set("views", "./client/views/pages");
+
+app.locals.CONFIG = global.CONFIG;
+
 if(global.CONFIG.https)
     app.set("trust proxy", "loopback"); //trust proxy for https.
                                         //if you are having problems with cookies not being set, you're probably doing something more complicated
