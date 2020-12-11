f6ec7ff0c208a73b9da6279d791bc73003d972fa
ereti <43418802+ereti@users.noreply.github.com>
2020-12-11 21:55:45 +0000

    Allow request of exact chart by chart hash

--- services/WebAPIService.js
+++ services/WebAPIService.js
@@ -2,28 +2,42 @@ class WebAPIService
 {
     async QueryCharts(req, res)
     {
-        const query = req.query.query; //lol
+        const hash = req.query.hash;
+        if(hash) //get specific chart by hash
+        {
+            if(typeof hash != "string") return res.status(400).json({error:"Bad hash provided"});
 
-        if(!query) return res.status(400).json({error:"No query provided."}); //don't just return all the charts because that might become a LOT of data. could change this though
-        if(typeof query != "string") return res.status(400).json({error:"Bad query provided."});
+            let chart = await global.DB.get("charts").findOne({chartHash: hash});
 
-        let search_query;
+            if(!chart) return res.status(404).json({error:"Chart not found."});
 
-        try {
-            search_query = new RegExp(`${query}`, "i");
-        } catch(e) {
-            return res.status(400).json({error:"Bad query provided."});
+            return res.status(200).json(chart);
         }
+        else
+        {
+            const query = req.query.query; //lol
 
-        const results = await global.DB.get("charts").find({
-            $or: [
-                {title: search_query},
-                {artist: search_query},
-                {effector: search_query}
-            ]
-        });
+            if(!query) return res.status(400).json({error:"No query provided."}); //don't just return all the charts because that might become a LOT of data. could change this though
+            if(typeof query != "string") return res.status(400).json({error:"Bad query provided."});
 
-        return res.status(200).json(results);
+            let search_query;
+
+            try {
+                search_query = new RegExp(`${query}`, "i");
+            } catch(e) {
+                return res.status(400).json({error:"Bad query provided."});
+            }
+
+            const results = await global.DB.get("charts").find({
+                $or: [
+                    {title: search_query},
+                    {artist: search_query},
+                    {effector: search_query}
+                ]
+            });
+
+            return res.status(200).json(results);
+        }
     }
 
     async ChartScores(req, res)
