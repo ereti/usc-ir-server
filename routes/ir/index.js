2099b64ba830be14dac5d68fe21406e031b87390
ereti <43418802+ereti@users.noreply.github.com>
2020-12-12 04:11:31 +0000

    Change score submission endpoint to /scores

--- routes/ir/index.js
+++ routes/ir/index.js
@@ -20,7 +20,7 @@ router.get("/",                                     IRService.Heartbeat);
 router.get("/charts/:chartHash",                    IRService.ChartTracked);
 router.get("/charts/:chartHash/record",             IRService.Record);
 router.get("/charts/:chartHash/leaderboard",        IRService.Leaderboard);
-router.post("/score/submit",                        IRService.SubmitScore);
+router.post("/scores",                              IRService.SubmitScore);
 
 router.post("/replays", upload.single("replay"),    IRService.SubmitReplay);
 
