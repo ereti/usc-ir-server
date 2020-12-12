4782b3592d40655e71f04a3aabdc97a8bd0ed29c
ereti <43418802+ereti@users.noreply.github.com>
2020-12-12 00:57:30 +0000

    Merge branch replays (PR #3)
    * Add multer
    
    * Support uploading of replay files
    
    * Fix a minor CSS oddity

--- routes/ir/index.js
+++ routes/ir/index.js
@@ -4,14 +4,23 @@
 
 const express = require("express");
 const IRService = require("../../services/IRService.js");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: "replays/"
+});
+
+const upload = multer({storage, limits: {files: 1, fileSize: 512 * 1024}}) //512kb
 
 const router = express.Router();
 
 router.use(IRService.Authorize);
 
-router.get("/",                         IRService.Heartbeat);
-router.get("/charts/:chartHash",        IRService.ChartTracked);
-router.get("/charts/:chartHash/record", IRService.Record);
-router.post("/score/submit",            IRService.SubmitScore);
+router.get("/",                                     IRService.Heartbeat);
+router.get("/charts/:chartHash",                    IRService.ChartTracked);
+router.get("/charts/:chartHash/record",             IRService.Record);
+router.post("/score/submit",                        IRService.SubmitScore);
+
+router.post("/replays", upload.single("replay"),    IRService.SubmitRecord);
 
 module.exports = router;
