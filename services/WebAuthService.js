ffe4fb5c546419d7e159fa0a7ad59098d9d1e3c2
ereti <43418802+ereti@users.noreply.github.com>
2021-01-27 19:41:51 +0000

    Merge feat/recaptcha (#6)
    * Add recaptcha for account registration (#4)
    
    * Make recaptcha optional
    
    * Document new config keys in readme
    
    Co-authored-by: Emil <emildraws@gmail.com>

--- services/WebAuthService.js
+++ services/WebAuthService.js
@@ -2,49 +2,59 @@ const Validation = require("../lib/validation.js");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const util = require("util");
+const fetch = require('node-fetch');
 
 const randomBytesPromise = util.promisify(crypto.randomBytes);
 
-class WebAuthService
-{
-    async Login(req, res)
-    {
+class WebAuthService {
+    async Login(req, res) {
         const user = req.session.user;
 
-        if(user) return res.status(400).json({invalid: "username", error: "Already logged in."});
+        if (user) return res.status(400).json({ invalid: "username", error: "Already logged in." });
 
         const username = req.body.username;
         const password = req.body.password;
 
-        if(!Validation.Username(username)) return res.status(400).json({invalid: "username", error:"Username must be 2-10 characters, and use only these characters: " + global.CONFIG.allowedUsernameCharacters});
-        if(!Validation.Password(password)) return res.status(400).json({invalid: "password", error:"Password must be 8-50 characters."});
+        if (!Validation.Username(username)) return res.status(400).json({ invalid: "username", error: "Username must be 2-10 characters, and use only these characters: " + global.CONFIG.allowedUsernameCharacters });
+        if (!Validation.Password(password)) return res.status(400).json({ invalid: "password", error: "Password must be 8-50 characters." });
 
-        const user_doc = await global.DB.get("users").findOne({username: username});
-        if(!user_doc) return res.status(401).json({invalid: "username", error: "User doesn't exist."});
+        const user_doc = await global.DB.get("users").findOne({ username: username });
+        if (!user_doc) return res.status(401).json({ invalid: "username", error: "User doesn't exist." });
 
         const correct_password = await bcrypt.compare(password, user_doc.password_hash);
 
-        if(!correct_password) return res.status(401).json({invalid: "password", error:"Incorrect password."});
+        if (!correct_password) return res.status(401).json({ invalid: "password", error: "Incorrect password." });
 
-        req.session.user = {username: user_doc.username};
+        req.session.user = { username: user_doc.username };
 
         return res.status(204).end();
     }
 
-    async Register(req, res)
-    {
+    async Register(req, res) {
         const user = req.session.user;
 
-        if(user) return res.status(400).json({invalid: "username", error:"Already logged in."});
+        if (user) return res.status(400).json({ invalid: "username", error: "Already logged in." });
 
         const username = req.body.username;
         const password = req.body.password;
+        const response = req.body.recaptcha;
 
-        if(!Validation.Username(username)) return res.status(400).json({invalid: "username", error:"Username must be 2-10 characters, and use only these characters: " + global.CONFIG.allowedUsernameCharacters});
-        if(!Validation.Password(password)) return res.status(400).json({invalid: "password", error:"Password must be 8-50 characters."});
+        if (!Validation.Username(username)) return res.status(400).json({ invalid: "username", error: "Username must be 2-10 characters, and use only these characters: " + global.CONFIG.allowedUsernameCharacters });
+        if (!Validation.Password(password)) return res.status(400).json({ invalid: "password", error: "Password must be 8-50 characters." });
 
-        const user_doc = await global.DB.get("users").findOne({username: username});
-        if(user_doc) return res.status(409).json({invalid: "username", error:"Username already taken."});
+        if(global.CONFIG.recaptcha.enabled)
+        {
+            let recaptcha_body = "secret=" + encodeURI(global.CONFIG.recaptcha.recaptchaSecret) + "&response=" + encodeURI(response);
+            let recaptcha_response = await fetch("https://www.google.com/recaptcha/api/siteverify?" + recaptcha_body, {
+                method: "POST"
+            });
+
+            const recaptcha_result = await recaptcha_response.json();
+            if (!recaptcha_result.success) return res.status(400).json({ invalid: "recaptcha", error: "reCaptcha challenge failed" });
+        }
+
+        const user_doc = await global.DB.get("users").findOne({ username: username });
+        if (user_doc) return res.status(409).json({ invalid: "username", error: "Username already taken." });
 
         const hash = await bcrypt.hash(password, global.CONFIG.hashWorkFactor);
 
@@ -53,9 +63,7 @@ class WebAuthService
         //this is terrifying but it should only ever run once in reality
         do {
             token = (await randomBytesPromise(8)).toString("hex");
-
-
-        } while(await global.DB.get("users").findOne({token}));
+        } while (await global.DB.get("users").findOne({ token }));
 
         const user_profile = {
             username: username,
@@ -69,8 +77,7 @@ class WebAuthService
         return res.status(204).end();
     }
 
-    async Logout(req, res)
-    {
+    async Logout(req, res) {
         req.session.destroy();
 
         return res.redirect("/");
